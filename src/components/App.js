08cad1755d20e4cbb5ade6f93a985fb0254c6e6b
RJTmini <redbas3@gmail.com>
2023-05-26 10:09:17 +0900

    ing

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fBase";
+import { onAuthStateChanged } from "firebase/auth";
+
+function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    onAuthStateChanged(authService, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Init..."}
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/Auth.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { authService } from "fBase";
+
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    let data;
+    if (newAccount) {
+      await createUserWithEmailAndPassword(authService, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      await signInWithEmailAndPassword(authService, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    console.log(data);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
