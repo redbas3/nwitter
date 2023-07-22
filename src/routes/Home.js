d621d759b3407ed3c9bc3e0a5dec10853b6b23b5
RJTmini <redbas3@gmail.com>
2023-07-22 19:27:07 +0900

    #3.2 end

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dbService } from "fBase";
+import { addDoc, getDocs, collection } from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  const getNweets = async () => {
+    const querySnapshot = await getDocs(collection(dbService, "nweets"));
+    querySnapshot.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await addDoc(collection(dbService, "nweets"), {
+        nweet,
+        createAt: Date.now(),
+      });
+      setNweet("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const onChange = (event) => {
+    event.preventDefault();
+
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={nweet}
+          type="input"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+        <div>
+          {nweets.map((nweet) => (
+            <div key={nweet.id}>
+              <h4>{nweet.nweet}</h4>
+            </div>
+          ))}
+        </div>
+      </form>
+    </div>
+  );
+};
+
 export default Home;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { authService } from "fBase";
+import { signOut } from "firebase/auth";
 
-export default <span>Profile</span>;
+function Profile() {
+  const history = useNavigate();
+  const onLogOutClick = () => {
+    signOut(authService)
+      .then(() => {
+        history("/");
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+}
+
+export default Profile;
