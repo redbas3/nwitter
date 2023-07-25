e6b6bcfff58b2745b9ae5365120dfa668b84d31d
RJTmini <redbas3@gmail.com>
2023-07-25 11:16:17 +0900

    #5.1 end

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-const Navigation = () => <nav>
-  <ul>
-    <li><Link to="/">Home</Link></li>
-    <li><Link to="/profile">My Profile</Link></li>
-  </ul>
-</nav>
+const Navigation = ({ userObj }) => (
+  <nav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/profile">{userObj.displayName}' Profile</Link>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Navigation;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { authService, dbService } from "fBase";
+import { updateProfile } from "firebase/auth";
 import { signOut } from "firebase/auth";
 import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 
 function Profile({ setUserObj, userObj }) {
   const history = useNavigate();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     signOut(authService)
       .then(() => {
@@ -35,8 +37,33 @@ function Profile({ setUserObj, userObj }) {
     getMyNweets();
   }, []);
 
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await updateProfile(authService.currentUser, {
+        displayName: newDisplayName,
+      });
+    }
+  };
+
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
